35ecea269bd37d843fbf5beb9dc2b9483497f324
Leanne <lrteeter@gmail.com>
2015-07-20 01:06:00 -0700

    Sudoku

--- Sudoku.m
+++ Sudoku.m
@@ -1,14 +1,11 @@
+Restorepuzzle = 0;
 if exist('done','var')
     if done == 0
         str = input('Restore puzzle from last session? y/n: ','s');
         if strcmp(str,'y') == 1 || strcmp(str,'Y') == 1 || strcmp(str,'yes') == 1 || strcmp(str,'Yes') == 1
             Restorepuzzle = 1;
-        else
-            Restorepuzzle = 0;
         end
     end
-else
-    Restorepuzzle = 0;
 end
 clc;
 
@@ -182,45 +179,48 @@ while done == 0;
     while v ==100
         str = input('Row : ','s');
         i = str2double(str);
-        if i == 0
-            disp(' ');
-            str = input('Reset? y/n: ','s');
-            if strcmp(str,'y') == 1 || strcmp(str,'Y') == 1 || strcmp(str,'yes') == 1 || strcmp(str,'Yes') == 1
-                reset = 1;
-                UserGrid = 0*rand(9);
-                TotalGrid = ComputerGrid + UserGrid;
-                clc;
-                Puzzle = sprintf('     1  2  3   4  5  6   7  8  9\n    -----------------------------\n 1 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 2 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 3 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 4 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 5 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 6 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 7 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 8 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 9 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------',TotalGrid(1,1),TotalGrid(1,2),TotalGrid(1,3),TotalGrid(1,4),TotalGrid(1,5),TotalGrid(1,6),TotalGrid(1,7),TotalGrid(1,8),TotalGrid(1,9),TotalGrid(2,1),TotalGrid(2,2),TotalGrid(2,3),TotalGrid(2,4),TotalGrid(2,5),TotalGrid(2,6),TotalGrid(2,7),TotalGrid(2,8),TotalGrid(2,9),TotalGrid(3,1),TotalGrid(3,2),TotalGrid(3,3),TotalGrid(3,4),TotalGrid(3,5),TotalGrid(3,6),TotalGrid(3,7),TotalGrid(3,8),TotalGrid(3,9),TotalGrid(4,1),TotalGrid(4,2),TotalGrid(4,3),TotalGrid(4,4),TotalGrid(4,5),TotalGrid(4,6),TotalGrid(4,7),TotalGrid(4,8),TotalGrid(4,9),TotalGrid(5,1),TotalGrid(5,2),TotalGrid(5,3),TotalGrid(5,4),TotalGrid(5,5),TotalGrid(5,6),TotalGrid(5,7),TotalGrid(5,8),TotalGrid(5,9),TotalGrid(6,1),TotalGrid(6,2),TotalGrid(6,3),TotalGrid(6,4),TotalGrid(6,5),TotalGrid(6,6),TotalGrid(6,7),TotalGrid(6,8),TotalGrid(6,9),TotalGrid(7,1),TotalGrid(7,2),TotalGrid(7,3),TotalGrid(7,4),TotalGrid(7,5),TotalGrid(7,6),TotalGrid(7,7),TotalGrid(7,8),TotalGrid(7,9),TotalGrid(8,1),TotalGrid(8,2),TotalGrid(8,3),TotalGrid(8,4),TotalGrid(8,5),TotalGrid(8,6),TotalGrid(8,7),TotalGrid(8,8),TotalGrid(8,9),TotalGrid(9,1),TotalGrid(9,2),TotalGrid(9,3),TotalGrid(9,4),TotalGrid(9,5),TotalGrid(9,6),TotalGrid(9,7),TotalGrid(9,8),TotalGrid(9,9))
+        while i > 9 || i < 1 || floor(i) ~= i || reset == 1
+            if i == 0
+                i = 1; reset = 1;
+                disp(' ');
+                str = input('Reset? y/n: ','s');
+                if strcmp(str,'y') == 1 || strcmp(str,'Y') == 1 || strcmp(str,'yes') == 1 || strcmp(str,'Yes') == 1
+                    UserGrid = 0*rand(9);
+                    TotalGrid = ComputerGrid + UserGrid;
+                    clc;
+                    Puzzle = sprintf('     1  2  3   4  5  6   7  8  9\n    -----------------------------\n 1 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 2 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 3 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 4 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 5 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 6 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 7 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 8 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 9 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------',TotalGrid(1,1),TotalGrid(1,2),TotalGrid(1,3),TotalGrid(1,4),TotalGrid(1,5),TotalGrid(1,6),TotalGrid(1,7),TotalGrid(1,8),TotalGrid(1,9),TotalGrid(2,1),TotalGrid(2,2),TotalGrid(2,3),TotalGrid(2,4),TotalGrid(2,5),TotalGrid(2,6),TotalGrid(2,7),TotalGrid(2,8),TotalGrid(2,9),TotalGrid(3,1),TotalGrid(3,2),TotalGrid(3,3),TotalGrid(3,4),TotalGrid(3,5),TotalGrid(3,6),TotalGrid(3,7),TotalGrid(3,8),TotalGrid(3,9),TotalGrid(4,1),TotalGrid(4,2),TotalGrid(4,3),TotalGrid(4,4),TotalGrid(4,5),TotalGrid(4,6),TotalGrid(4,7),TotalGrid(4,8),TotalGrid(4,9),TotalGrid(5,1),TotalGrid(5,2),TotalGrid(5,3),TotalGrid(5,4),TotalGrid(5,5),TotalGrid(5,6),TotalGrid(5,7),TotalGrid(5,8),TotalGrid(5,9),TotalGrid(6,1),TotalGrid(6,2),TotalGrid(6,3),TotalGrid(6,4),TotalGrid(6,5),TotalGrid(6,6),TotalGrid(6,7),TotalGrid(6,8),TotalGrid(6,9),TotalGrid(7,1),TotalGrid(7,2),TotalGrid(7,3),TotalGrid(7,4),TotalGrid(7,5),TotalGrid(7,6),TotalGrid(7,7),TotalGrid(7,8),TotalGrid(7,9),TotalGrid(8,1),TotalGrid(8,2),TotalGrid(8,3),TotalGrid(8,4),TotalGrid(8,5),TotalGrid(8,6),TotalGrid(8,7),TotalGrid(8,8),TotalGrid(8,9),TotalGrid(9,1),TotalGrid(9,2),TotalGrid(9,3),TotalGrid(9,4),TotalGrid(9,5),TotalGrid(9,6),TotalGrid(9,7),TotalGrid(9,8),TotalGrid(9,9))
+                else
+                    clc;
+                    Puzzle = sprintf('     1  2  3   4  5  6   7  8  9\n    -----------------------------\n 1 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 2 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 3 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 4 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 5 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 6 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 7 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 8 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 9 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------',TotalGrid(1,1),TotalGrid(1,2),TotalGrid(1,3),TotalGrid(1,4),TotalGrid(1,5),TotalGrid(1,6),TotalGrid(1,7),TotalGrid(1,8),TotalGrid(1,9),TotalGrid(2,1),TotalGrid(2,2),TotalGrid(2,3),TotalGrid(2,4),TotalGrid(2,5),TotalGrid(2,6),TotalGrid(2,7),TotalGrid(2,8),TotalGrid(2,9),TotalGrid(3,1),TotalGrid(3,2),TotalGrid(3,3),TotalGrid(3,4),TotalGrid(3,5),TotalGrid(3,6),TotalGrid(3,7),TotalGrid(3,8),TotalGrid(3,9),TotalGrid(4,1),TotalGrid(4,2),TotalGrid(4,3),TotalGrid(4,4),TotalGrid(4,5),TotalGrid(4,6),TotalGrid(4,7),TotalGrid(4,8),TotalGrid(4,9),TotalGrid(5,1),TotalGrid(5,2),TotalGrid(5,3),TotalGrid(5,4),TotalGrid(5,5),TotalGrid(5,6),TotalGrid(5,7),TotalGrid(5,8),TotalGrid(5,9),TotalGrid(6,1),TotalGrid(6,2),TotalGrid(6,3),TotalGrid(6,4),TotalGrid(6,5),TotalGrid(6,6),TotalGrid(6,7),TotalGrid(6,8),TotalGrid(6,9),TotalGrid(7,1),TotalGrid(7,2),TotalGrid(7,3),TotalGrid(7,4),TotalGrid(7,5),TotalGrid(7,6),TotalGrid(7,7),TotalGrid(7,8),TotalGrid(7,9),TotalGrid(8,1),TotalGrid(8,2),TotalGrid(8,3),TotalGrid(8,4),TotalGrid(8,5),TotalGrid(8,6),TotalGrid(8,7),TotalGrid(8,8),TotalGrid(8,9),TotalGrid(9,1),TotalGrid(9,2),TotalGrid(9,3),TotalGrid(9,4),TotalGrid(9,5),TotalGrid(9,6),TotalGrid(9,7),TotalGrid(9,8),TotalGrid(9,9))
+                end
             else
-                clc;
-                Puzzle = sprintf('     1  2  3   4  5  6   7  8  9\n    -----------------------------\n 1 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 2 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 3 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 4 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 5 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 6 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------\n 7 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 8 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n 9 | %d  %d  %d | %d  %d  %d | %d  %d  %d |\n    -----------------------------',TotalGrid(1,1),TotalGrid(1,2),TotalGrid(1,3),TotalGrid(1,4),TotalGrid(1,5),TotalGrid(1,6),TotalGrid(1,7),TotalGrid(1,8),TotalGrid(1,9),TotalGrid(2,1),TotalGrid(2,2),TotalGrid(2,3),TotalGrid(2,4),TotalGrid(2,5),TotalGrid(2,6),TotalGrid(2,7),TotalGrid(2,8),TotalGrid(2,9),TotalGrid(3,1),TotalGrid(3,2),TotalGrid(3,3),TotalGrid(3,4),TotalGrid(3,5),TotalGrid(3,6),TotalGrid(3,7),TotalGrid(3,8),TotalGrid(3,9),TotalGrid(4,1),TotalGrid(4,2),TotalGrid(4,3),TotalGrid(4,4),TotalGrid(4,5),TotalGrid(4,6),TotalGrid(4,7),TotalGrid(4,8),TotalGrid(4,9),TotalGrid(5,1),TotalGrid(5,2),TotalGrid(5,3),TotalGrid(5,4),TotalGrid(5,5),TotalGrid(5,6),TotalGrid(5,7),TotalGrid(5,8),TotalGrid(5,9),TotalGrid(6,1),TotalGrid(6,2),TotalGrid(6,3),TotalGrid(6,4),TotalGrid(6,5),TotalGrid(6,6),TotalGrid(6,7),TotalGrid(6,8),TotalGrid(6,9),TotalGrid(7,1),TotalGrid(7,2),TotalGrid(7,3),TotalGrid(7,4),TotalGrid(7,5),TotalGrid(7,6),TotalGrid(7,7),TotalGrid(7,8),TotalGrid(7,9),TotalGrid(8,1),TotalGrid(8,2),TotalGrid(8,3),TotalGrid(8,4),TotalGrid(8,5),TotalGrid(8,6),TotalGrid(8,7),TotalGrid(8,8),TotalGrid(8,9),TotalGrid(9,1),TotalGrid(9,2),TotalGrid(9,3),TotalGrid(9,4),TotalGrid(9,5),TotalGrid(9,6),TotalGrid(9,7),TotalGrid(9,8),TotalGrid(9,9))
-            end
-        else
-            while i > 9 || i < 1 || floor(i) ~= i
-                disp(' ');disp('Must be a number 1 - 9.');disp(' ');
+                if reset == 0
+                    disp(' ');disp('Must be a number 1 - 9.');disp(' ');
+                end
                 str = input('Row : ','s');
                 i = str2double(str);
+                reset = 0;
             end
+        end
+        str = input('Column : ','s');
+        j = str2double(str);
+        while j > 9 || j < 1 || floor(j) ~= j
+            disp(' ');disp('Must be a number 1 - 9.');disp(' ');
             str = input('Column : ','s');
             j = str2double(str);
-            while j > 9 || j < 1 || floor(j) ~= j
-                disp(' ');disp('Must be a number 1 - 9.');disp(' ');
-                str = input('Column : ','s');
-                j = str2double(str);
-            end
-            if ComputerGrid(i,j) ~= 0;
-                disp(' '); disp('Cannot change an original number.'); disp(' ');
-            else
+        end
+        if ComputerGrid(i,j) ~= 0;
+            disp(' '); disp('Cannot change an original number.'); disp(' ');
+        else
+            str = input('Value : ','s');
+            v = str2double(str);
+            while v > 9 || v < 0 || floor(v) ~= v
+                disp(' ');disp('Must be a number 0 - 9.');disp(' ');
                 str = input('Value : ','s');
                 v = str2double(str);
-                while v > 9 || v < 0 || floor(v) ~= v
-                    disp(' ');disp('Must be a number 0 - 9.');disp(' ');
-                    str = input('Value : ','s');
-                    v = str2double(str);
-                end
-                UserGrid(i,j) = v;
-                TotalGrid = UserGrid + ComputerGrid;
             end
+            UserGrid(i,j) = v;
+            TotalGrid = UserGrid + ComputerGrid;
         end
     end
     clc;
